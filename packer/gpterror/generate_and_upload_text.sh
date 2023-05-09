68b147e465111f2eda80eed8fa02267688f399e5
Rodrigo <rrebelatto@outlook.com>
2023-05-09 18:05:09 -0300

    make stories... weirder

--- packer/gpterror/generate_and_upload_text.sh
+++ packer/gpterror/generate_and_upload_text.sh
@@ -7,7 +7,7 @@ if [ $file_count -lt 99 ]; then
   -H "Authorization: Bearer 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" \
   -d '{
     "model": "gpt-3.5-turbo",
-    "messages": [{"role": "user", "content": "me conta um história de terror"}],
+    "messages": [{"role": "user", "content": "create a fictional horror story choosing randonly two of this themes: demons, ghosts, witches, pychos, madness, cosmic horror, dystopic futures, killer robots, cybernetic treats, biological treats, apocalypse or serial killers. The story must have at least one thousand words."}],
     "temperature": 0.7
   }' | python3 -c 'import sys, json; print(json.load(sys.stdin)["choices"][0]["message"]["content"])')
 
